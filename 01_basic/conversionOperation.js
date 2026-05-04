011a1fc662c0556e2fd78f1648f8b76d06f87c8c
Dinesh Kumar <dineshkumarmadhussm@gmail.com>
2026-05-04 19:24:05 +0000

    conversionOperation

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,10 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//console.log(typeof negValue);
+
+let srt1 = "hello"
+let str2 ="dinesh"
+let str3 = str1 + str2
+console.log(str3)
+
